6f7a179767e32cbe5285659e118b7f3b32800ede
victor <idimahvictor9@gmail.com>
2026-08-22 11:21:26 +0100

    temp debug script

---
+++ check_db2.py
@@ -0,0 +1,14 @@
+﻿import os
+import mysql.connector as m
+
+c = m.connect(
+    user=os.environ["MYSQLUSER"],
+    password=os.environ["MYSQLPASSWORD"],
+    host=os.environ["MYSQLHOST"],
+    port=int(os.environ["MYSQLPORT"]),
+    database=os.environ["MYSQL_DATABASE"]
+)
+cur = c.cursor()
+cur.execute("SELECT user, host FROM mysql.user WHERE user=%s", ("root",))
+print(cur.fetchall())
+c.close()
